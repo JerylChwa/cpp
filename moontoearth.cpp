0b3d6e38d05790892c679514fa2696aefb4728cc
Jeryl <jeryl0308@gmail.com>
2025-07-23 00:01:23 +0800

    moon to earth program weight

---
+++ moontoearth.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double moon_weight, earth_weight;
+    int count;
+
+    count = 0;
+
+    for (int earth_weight = 1; earth_weight <= 100.0; earth_weight++) {
+        moon_weight = 0.17 * earth_weight;
+        cout << earth_weight << " earthweight is " << moon_weight << " moonweight" << "\n";
+        count++;
+        if (count == 25) {
+            cout << "\n";
+            count = 0;
+        }
+    }
+    return 0;
+}
